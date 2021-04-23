3428f5cbe26f79f3341af05533427874fe835891
Jeffery <522023893@qq.com>
2021-04-23 18:19:39 +0800

    尝试引入模板

---
+++ app/javascript/demo/index.js
@@ -0,0 +1,6 @@
+require("./chart-area-demo");
+require("./chart-bar-demo");
+require("./datatables-demo");
+require("./sb-admin-2");
+require("../vendor/chart.js/Chart");
+require("../vendor/chart.js/Chart.bundle")
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,8 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-//require Chart.min
+//= require Chart.min
+//= require Chart.js
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
@@ -13,5 +14,4 @@ Turbolinks.start()
 ActiveStorage.start()
 
 import "../bootstrap-uri"
-
-
+import "../demo"
